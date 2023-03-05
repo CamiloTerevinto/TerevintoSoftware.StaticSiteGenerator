a27869a509003e28fc8b21df1dfc84a059a74c32
Camilo Terevinto <camiloterevinto@outlook.com>
2023-03-05 18:06:36 +0000

    Release 1.3.5:
    Delete accidentally pushed code.

--- TerevintoSoftware.StaticSiteGenerator.Tool/Program.cs
+++ TerevintoSoftware.StaticSiteGenerator.Tool/Program.cs
@@ -3,16 +3,4 @@ using TerevintoSoftware.StaticSiteGenerator.Tool;
 
 var rootCommand = StaticSiteGenerationOptionsBinder.BuildRootCommand();
 
-args = new[]
-{
-    "--project",
-    "D:\\Source\\CT.Website\\CT.Website",
-    "--output",
-    "D:\\Source\\CT.Website\\static",
-    "--route-casing",
-    "KebabCase",
-    "--assembly",
-    "bin/Release/net6.0/CT.Website.dll"
-};
-
 return await rootCommand.InvokeAsync(args);
